125855fbc63d9e4b1f2f74c219bcac2fbaf69be0
Panzipride <118266922+Panzipride@users.noreply.github.com>
2022-12-25 18:55:09 +0800

    Update mode-bot.js

--- plugins/mode-bot.js
+++ plugins/mode-bot.js
@@ -12,13 +12,13 @@ let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
  await conn.sendButton(m.chat, `*Hai Ngab*
-ɪ'ᴍ ɴᴇxʙᴏᴛᴢ ʏᴀɴɢ ᴅɪ ʙᴜᴀᴛ ɢᴀᴋ ɴɪᴀᴛ ᴏʟᴇʜ ᴘᴜᴛʀᴀᴍᴏᴅz ᴋᴀʀᴇɴᴀ ᴍᴀʟᴇꜱ ʀᴇᴄᴏᴅᴇ ᴊᴀᴅɪ ᴘᴀᴋᴇ ᴀᴊᴀ ᴀᴘᴀ ʏᴀɴɢ ᴀᴅᴀ ʏᴀʜ.
+I'm AtsairBotz Yang Di Buat Gak Niat Oleh Atsaiir kul karena males recode jadi Pake aja yang ada.
 Kalau Ada Yg Error Bisa Langsung Report Ke Owner.\n`,wm + '\n\n' + botdate, thumbdoc, [['Aʟʟ Mᴇɴᴜ','.? all'],['Lɪsᴛ Mᴇɴᴜ','.siuuu']], m, {
 contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
-                        title: `A PUTRA🎧 : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+                        title: `A ATSAIR🎧 : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
                         body: `Join Grup`,          previewType: 0,
                         thumbnail: await (await fetch(`https://i.ibb.co/jbHnwDr/20221218-011857.jpg`)).buffer(),
                         sourceUrl: 'https://chat.whatsapp.com/FR1KfE3OTcQCayiabos8Ff'
